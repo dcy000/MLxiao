a4530661e9a9329e5c807ef538f4bb8df3d0994e
han qi <2332971095@qq.com>
2017-09-01 16:20:53 +0800

    update

--- app/src/main/java/com/example/han/referralproject/MainActivity.java
+++ app/src/main/java/com/example/han/referralproject/MainActivity.java
@@ -31,10 +31,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageView mImageView3;
     ImageView mImageView4;
     ImageView mImageView5;
-   // SharedPreferences sharedPreferences;
-  //  SharedPreferences sharedPreferences1;
-
-    SharedPreferences sharedPreferences;
+    ImageView mImageView6;
+    ImageView mImageView7;
 
 
     @Override
@@ -43,62 +41,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
-        mImageView1 = (ImageView) findViewById(R.id.health_test);
-        mImageView2 = (ImageView) findViewById(R.id.doctor_remind);
-        mImageView3 = (ImageView) findViewById(R.id.person_info);
-        mImageView4 = (ImageView) findViewById(R.id.face_to_face);
-        mImageView5 = (ImageView) findViewById(R.id.robot_conversion);
+        mImageView1 = (ImageView) findViewById(R.id.conversation);
+        mImageView2 = (ImageView) findViewById(R.id.shopping);
+        mImageView3 = (ImageView) findViewById(R.id.zixun);
+        mImageView4 = (ImageView) findViewById(R.id.per_info);
+        mImageView5 = (ImageView) findViewById(R.id.test);
+        mImageView6 = (ImageView) findViewById(R.id.classes);
+        mImageView7 = (ImageView) findViewById(R.id.news);
 
         mImageView1.setOnClickListener(this);
         mImageView2.setOnClickListener(this);
         mImageView3.setOnClickListener(this);
         mImageView4.setOnClickListener(this);
         mImageView5.setOnClickListener(this);
+        mImageView6.setOnClickListener(this);
+        mImageView7.setOnClickListener(this);
 
-        sharedPreferences = getSharedPreferences(com.megvii.faceppidcardui.util.ConstantData.SHARED_FILE_NAME4, Context.MODE_PRIVATE);
-
-        Log.e("============", sharedPreferences.getString("mAuthid", ""));
-
-
-        //sharedPreferences = getSharedPreferences(ConstantData.SHARED_FILE_NAME, Context.MODE_PRIVATE);
-
-     /*   SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("name", "123");
-        editor.putString("phone", "123");
-        editor.commit();
-
-
-        sharedPreferences1 = getSharedPreferences(ConstantData.SHARED_FILE_NAMES, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor1 = sharedPreferences1.edit();
-        editor1.putString("name", "456");
-        editor1.putString("phone", "456");
-        editor1.commit();*/
-        //    showNormal();
-
-    }
-
-    public void showNormal() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setIcon(R.drawable.user);
-
-        builder.setTitle("虞美人");
-        builder.setMessage("雕栏玉砌应犹在，只是朱颜改。问君能有多少愁？恰似一江春水向东流。");
-
-
-        builder.setNegativeButton("取消", new Dialog.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(getApplicationContext(), i + "--setNegativeButton", Toast.LENGTH_SHORT).show();
-            }
-        });
-        builder.setPositiveButton("确定", new Dialog.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(), i + "--setPositiveButton", Toast.LENGTH_SHORT).show();
-                    }
-                }
-        );
-        builder.show();
 
     }
 
@@ -107,19 +65,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         Intent intent = new Intent();
         switch (v.getId()) {
-            case R.id.health_test:
+            case R.id.conversation:
                 intent.setClass(getApplicationContext(), VideoDemo.class);
                 startActivity(intent);
                 break;
-            case R.id.doctor_remind:
+            case R.id.shopping:
                 break;
-            case R.id.person_info:
+            case R.id.zixun:
                 intent.setClass(getApplicationContext(), PerInfoActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.face_to_face:
+            case R.id.per_info:
                 break;
-            case R.id.robot_conversion:
+            case R.id.test:
+                intent.setClass(getApplicationContext(), SpeechSynthesisActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.classes:
+                intent.setClass(getApplicationContext(), SpeechSynthesisActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.news:
                 intent.setClass(getApplicationContext(), SpeechSynthesisActivity.class);
                 startActivity(intent);
                 break;
@@ -129,11 +95,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStart() {
         super.onStart();
-      /*  Log.e("=============", sharedPreferences.getString("name", ""));
-        Log.e("=============", sharedPreferences.getString("phone", ""));
-        Log.e("=============", sharedPreferences1.getString("name", ""));
-        Log.e("=============", sharedPreferences1.getString("phone", ""));
-*/
 
     }
 }
