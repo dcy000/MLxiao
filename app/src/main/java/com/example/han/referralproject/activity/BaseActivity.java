fc431753db14f2b7ae00759d8bef692e9ee2f68c
dcy <dcy@dcydeMacBook-Pro.local>
2017-09-28 16:12:02 +0800

    update

--- app/src/main/java/com/example/han/referralproject/activity/BaseActivity.java
+++ app/src/main/java/com/example/han/referralproject/activity/BaseActivity.java
@@ -10,16 +10,27 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.example.han.referralproject.R;
 import com.example.han.referralproject.speech.setting.TtsSettings;
+import com.example.han.referralproject.speech.util.JsonParser;
 import com.iflytek.cloud.ErrorCode;
 import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.RecognizerListener;
+import com.iflytek.cloud.RecognizerResult;
 import com.iflytek.cloud.SpeechConstant;
 import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechRecognizer;
 import com.iflytek.cloud.SpeechSynthesizer;
 import com.iflytek.cloud.SynthesizerListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class BaseActivity extends AppCompatActivity {
     protected Context mContext;
@@ -32,7 +43,9 @@ public class BaseActivity extends AppCompatActivity {
     // 引擎类型
     private String mEngineType = SpeechConstant.TYPE_CLOUD;
     private SharedPreferences mTtsSharedPreferences;
+    private SpeechRecognizer mIat;
     private Handler mDelayHandler = new Handler();
+    private HashMap<String, String> mIatResults = new LinkedHashMap<String, String>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,10 +53,10 @@ public class BaseActivity extends AppCompatActivity {
         mContext = this;
         mResources = getResources();
 
+        mIat = SpeechRecognizer.createRecognizer(this, mTtsInitListener);
         mTts = SpeechSynthesizer.createSynthesizer(mContext, mTtsInitListener);
         mTtsSharedPreferences = getSharedPreferences(TtsSettings.PREFER_NAME, MODE_PRIVATE);
     }
-
 
     private InitListener mTtsInitListener = new InitListener() {
         @Override
@@ -53,6 +66,7 @@ public class BaseActivity extends AppCompatActivity {
             } else {
                 // 设置参数
                 setParam();
+                setListenerParam();
                 // 初始化成功，之后可以调用startSpeaking方法
                 // 注：有的开发者在onCreate方法中创建完合成对象之后马上就调用startSpeaking进行合成，
                 // 正确的做法是将onCreate中的startSpeaking调用移至这里
@@ -75,6 +89,70 @@ public class BaseActivity extends AppCompatActivity {
 //            }
 //        }, 500);
     }
+
+    protected void startSpeakListener(){
+        if (mIat == null){
+            return;
+        }
+        setListenerParam();
+        if (!mIat.isListening()){
+            mIat.startListening(mIatListener);
+        }
+    }
+
+    protected void onSpeakListenerResult(String result){};
+
+    private RecognizerListener mIatListener = new RecognizerListener() {
+        @Override
+        public void onVolumeChanged(int i, byte[] bytes) {
+        }
+
+        @Override
+        public void onBeginOfSpeech() {
+            Log.i("speak", "speakbegin          ");
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            Log.i("speak", "speakend          ");
+        }
+
+        @Override
+        public void onResult(RecognizerResult recognizerResult, boolean isLast) {
+//            if (!isLast){
+//                return;
+//            }
+            mIat.startListening(mIatListener);
+            mIatResults.clear();
+            String text = JsonParser.parseIatResult(recognizerResult.getResultString());
+            String sn = null;
+            // 读取json结果中的sn字段
+            try {
+                JSONObject resultJson = new JSONObject(recognizerResult.getResultString());
+                sn = resultJson.optString("sn");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            mIatResults.put(sn, text);
+            StringBuffer resultBuffer = new StringBuffer();
+            for (String key : mIatResults.keySet()) {
+                resultBuffer.append(mIatResults.get(key));
+            }
+            if (!TextUtils.isEmpty(resultBuffer.toString())){
+                onSpeakListenerResult(resultBuffer.toString());
+            }
+        }
+
+        @Override
+        public void onError(SpeechError speechError) {
+            mIat.startListening(mIatListener);
+            Log.i("speak", "error          " + speechError.getErrorDescription());
+        }
+
+        @Override
+        public void onEvent(int i, int i1, int i2, Bundle bundle) {
+        }
+    };
 
     private SynthesizerListener mTtsListener = new SynthesizerListener() {
 
@@ -112,6 +190,42 @@ public class BaseActivity extends AppCompatActivity {
     };
 
     protected void onActivitySpeakFinish(){}
+
+
+    private void setListenerParam(){
+        // 清空参数
+        mIat.setParameter(SpeechConstant.PARAMS, null);
+
+        // 设置听写引擎
+        mIat.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType);
+        // 设置返回结果格式
+        mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");
+
+        String lag = mTtsSharedPreferences.getString("iat_language_preference", "mandarin");
+        if (lag.equals("en_us")) {
+            // 设置语言
+            mIat.setParameter(SpeechConstant.LANGUAGE, "en_us");
+        } else {
+            // 设置语言
+            mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+            // 设置语言区域
+            mIat.setParameter(SpeechConstant.ACCENT, lag);
+        }
+
+        // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
+        mIat.setParameter(SpeechConstant.VAD_BOS, mTtsSharedPreferences.getString("iat_vadbos_preference", "5000"));
+
+        // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
+        mIat.setParameter(SpeechConstant.VAD_EOS, mTtsSharedPreferences.getString("iat_vadeos_preference", "500"));
+
+        // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
+        mIat.setParameter(SpeechConstant.ASR_PTT, mTtsSharedPreferences.getString("iat_punc_preference", "0"));
+
+        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
+        // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
+        mIat.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");
+        mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory() + "/msc/iat.wav");
+    }
 
     /**
      * 参数设置
@@ -156,6 +270,10 @@ public class BaseActivity extends AppCompatActivity {
         if (mTts != null) {
             mTts.stopSpeaking();
             mTts.destroy();
+        }
+        if (null != mIat) {
+            mIat.cancel();
+            mIat.destroy();
         }
     }
 
