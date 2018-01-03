dc23060e23ac3293294a2a18c4aa5baca3a2ed6d
guozhiqiang123 <774550196@qq.com>
2018-01-03 15:56:32 +0800

    finish

--- app/src/main/java/com/example/han/referralproject/speechsynthesis/SpeechSynthesisActivity.java
+++ app/src/main/java/com/example/han/referralproject/speechsynthesis/SpeechSynthesisActivity.java
@@ -203,6 +203,7 @@ public class SpeechSynthesisActivity extends BaseActivity implements View.OnClic
                     hidePlayingFragment();
                     mImageView.setClickable(true);
                 }
+                finish();
               }
         });
         initLayout();
