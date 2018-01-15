e4b0a9523aab6c0a2a51ffc61f85062156469f3c
dcy <dcy@dcydeMacBook-Pro.local>
2018-01-15 22:44:41 +0800

    update

--- app/src/main/java/com/example/han/referralproject/speechsynthesis/SpeechSynthesisActivity.java
+++ app/src/main/java/com/example/han/referralproject/speechsynthesis/SpeechSynthesisActivity.java
@@ -679,19 +679,6 @@ public class SpeechSynthesisActivity extends BaseActivity implements View.OnClic
                 startActivity(intent);
                 return;
             }
-            if (inSpell.matches(REGEX_SEE_DOCTOR)) {
-                DiseaseUser diseaseUser=new DiseaseUser(
-                        LocalShared.getInstance(this).getUserName(),
-                        LocalShared.getInstance(this).getSex().equals("男")? 1:2,
-                        Integer.parseInt(LocalShared.getInstance(this).getUserAge())*12,
-                        LocalShared.getInstance(this).getUserPhoto()
-                );
-                String currentUser= new Gson().toJson(diseaseUser);
-                Intent intent = new Intent(this, com.witspring.unitbody.ChooseMemberActivity.class);
-                intent.putExtra("currentUser", currentUser);
-                startActivity(intent);
-                return;
-            }
 
             if (inSpell.matches(".*(jiankangketang|jiemu|shipin|dianshi).*")) {
                 VideoListActivity.launch(SpeechSynthesisActivity.this, 0);
@@ -1102,6 +1089,7 @@ public class SpeechSynthesisActivity extends BaseActivity implements View.OnClic
 
             str1 = string.getReceive().getOutput();
         }
+
         if (str1 != null) {
 
             if (getString(R.string.speak_null).equals(str1)) {
