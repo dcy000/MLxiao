01d41125b2b1ce20dd0935e58a89cf2852e61e2a
zhangjiahuashan <1191547491@qq.com>
2018-02-07 19:35:55 +0800

    隐藏语言设置

--- app/src/main/java/com/example/han/referralproject/speechsynthesis/SpeechSynthesisActivity.java
+++ app/src/main/java/com/example/han/referralproject/speechsynthesis/SpeechSynthesisActivity.java
@@ -168,9 +168,6 @@ public class SpeechSynthesisActivity extends BaseActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setShowVoiceView(true);
         setContentView(R.layout.activity_speech_synthesis);
-
-        speak("主人,来和我聊天吧");
-
         rand = new Random();
         sharedPreferences = getSharedPreferences(ConstantData.DOCTOR_MSG, Context.MODE_PRIVATE);
         mImageView = (ImageView) findViewById(R.id.iat_recognizes);
@@ -1302,10 +1299,10 @@ public class SpeechSynthesisActivity extends BaseActivity implements View.OnClic
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
             switch (requestCode){
                 case TO_MUSICPLAY:
-                    speak("主人，想听更多歌曲，请告诉我！");
+                    speak("主人，想听更多歌曲，请告诉我！",isDefaultParam);
                     break;
                 case TO_STORY:
-                    speak("主人，我讲的故事好听吗？");
+                    speak("主人，我讲的故事好听吗？",isDefaultParam);
                     break;
             }
         super.onActivityResult(requestCode, resultCode, data);
