83ba1b5786cf781aaf6cd9e4cb7a0892131a192f
zhangjiahuashan <1191547491@qq.com>
2018-03-06 17:03:53 +0800

    百科页面

---
+++ app/src/main/java/com/example/han/referralproject/tool/BaiKeActivtiy.java
@@ -0,0 +1,150 @@
+package com.example.han.referralproject.tool;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.han.referralproject.R;
+import com.example.han.referralproject.speech.util.JsonParser;
+import com.example.han.referralproject.tool.xfparsebean.BaiKeBean;
+import com.example.han.referralproject.tool.xfparsebean.DreamBean;
+import com.example.han.referralproject.voice.SpeechRecognizerHelper;
+import com.iflytek.cloud.RecognizerListener;
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechRecognizer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class BaiKeActivtiy extends AppCompatActivity {
+
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.tv_demo1)
+    TextView tvDemo1;
+    @BindView(R.id.tv_demo2)
+    TextView tvDemo2;
+    @BindView(R.id.tv_demo3)
+    TextView tvDemo3;
+    @BindView(R.id.iv_yuyin)
+    ImageView ivYuyin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai_ke_activtiy);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.tv_title, R.id.tv_demo1, R.id.tv_demo2, R.id.tv_demo3, R.id.iv_yuyin})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_title:
+                break;
+            case R.id.tv_demo1:
+                String demo1 = tvDemo1.getText().toString();
+                getDreamData(demo1);
+                break;
+            case R.id.tv_demo2:
+                String demo2 = tvDemo2.getText().toString();
+                getDreamData(demo2);
+                break;
+            case R.id.tv_demo3:
+                String demo3 = tvDemo3.getText().toString();
+                getDreamData(demo3);
+                break;
+            case R.id.iv_yuyin:
+                startListener();
+                break;
+        }
+    }
+
+    private void startListener() {
+        SpeechRecognizer speechRecognizer = SpeechRecognizerHelper.initSpeechRecognizer(this);
+        speechRecognizer.startListening(new RecognizerListener() {
+            @Override
+            public void onVolumeChanged(int i, byte[] bytes) {
+
+            }
+
+            @Override
+            public void onBeginOfSpeech() {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onResult(RecognizerResult recognizerResult, boolean b) {
+                dealData(recognizerResult, b);
+            }
+
+            @Override
+            public void onError(SpeechError speechError) {
+
+            }
+
+            @Override
+            public void onEvent(int i, int i1, int i2, Bundle bundle) {
+
+            }
+        });
+    }
+
+    private void dealData(RecognizerResult recognizerResult, boolean isLast) {
+        if (isLast) {
+            getDreamData(printResult(recognizerResult).toString());
+        }
+    }
+
+    private void getDreamData(final String result) {
+        XFSkillApi.getSkillData(result, new XFSkillApi.getDataListener() {
+            @Override
+            public void onSuccess(final Object anwser, final String briefly) {
+
+            }
+
+            @Override
+            public void onSuccess(Object anwser) {
+                List<BaiKeBean> data = (List<BaiKeBean>) anwser;
+                BaikeResultActivity.startMe(BaiKeActivtiy.this,data,result);
+            }
+        });
+    }
+
+    private HashMap<String, String> xfResult = new LinkedHashMap<String, String>();
+
+    private StringBuffer printResult(RecognizerResult results) {
+        String text = JsonParser.parseIatResult(results.getResultString());
+        String sn = null;
+        // 读取json结果中的sn字段
+        try {
+            JSONObject resultJson = new JSONObject(results.getResultString());
+            sn = resultJson.optString("sn");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        xfResult.put(sn, text);
+
+        StringBuffer resultBuffer = new StringBuffer();
+        for (String key : xfResult.keySet()) {
+            resultBuffer.append(xfResult.get(key));
+        }
+        return resultBuffer;
+
+    }
+}
---
+++ app/src/main/java/com/example/han/referralproject/tool/BaikeResultActivity.java
@@ -0,0 +1,62 @@
+package com.example.han.referralproject.tool;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.han.referralproject.R;
+import com.example.han.referralproject.tool.xfparsebean.BaiKeBean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class BaikeResultActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.tv_question)
+    TextView tvQuestion;
+    @BindView(R.id.img_pic)
+    ImageView imgPic;
+    @BindView(R.id.mt_result)
+    MixtureTextView mtResult;
+    private List<BaiKeBean> data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baike_result);
+        ButterKnife.bind(this);
+        data = (List<BaiKeBean>) getIntent().getSerializableExtra("data");
+        initView();
+
+    }
+
+    private void initView() {
+        Glide.with(this).load(data.get(0).url).into(imgPic);
+        tvQuestion.setText(getIntent().getStringExtra("question"));
+        mtResult.setText(data.get(0).summary);
+    }
+
+
+    public static void startMe(Context context, List<BaiKeBean> data,String question) {
+        Intent intent = new Intent(context, BaikeResultActivity.class);
+        intent.putExtra("data", (Serializable) data);
+        intent.putExtra("question", question);
+        context.startActivity(intent);
+    }
+
+    @OnClick(R.id.tv_title)
+    public void onViewClicked() {
+        finish();
+    }
+}
