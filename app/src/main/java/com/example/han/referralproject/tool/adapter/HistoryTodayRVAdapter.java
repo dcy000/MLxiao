055cdea1430aa28d950c60ecbfd84b8227a3e005
zhangjiahuashan <1191547491@qq.com>
2018-03-08 09:23:19 +0800

    历史上的今天,展开语折叠

--- app/src/main/java/com/example/han/referralproject/tool/adapter/HistoryTodayRVAdapter.java
+++ app/src/main/java/com/example/han/referralproject/tool/adapter/HistoryTodayRVAdapter.java
@@ -2,11 +2,14 @@ package com.example.han.referralproject.tool.adapter;
 
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.example.han.referralproject.R;
 import com.example.han.referralproject.tool.ExpandableTextView;
+import com.example.han.referralproject.tool.MixtureTextView;
 import com.example.han.referralproject.tool.xfparsebean.HistoryTodayBean;
 
 import java.util.List;
@@ -21,15 +24,37 @@ public class HistoryTodayRVAdapter extends BaseQuickAdapter<HistoryTodayBean, Ba
     }
 
     @Override
-    protected void convert(final BaseViewHolder holder, HistoryTodayBean bean) {
+    protected void convert(final BaseViewHolder holder, final HistoryTodayBean bean) {
         holder.setText(R.id.tv_content, bean.description);
         holder.setText(R.id.tv_title, bean.title);
-        ExpandableTextView view =holder.getView(R.id.expandableTextView);
-        view.isOpen=bean.flag;
-        view.setClickListner(new ExpandableTextView.ClickListner() {
+        final ExpandableTextView close = holder.getView(R.id.expandableTextView);
+        final MixtureTextView open = holder.getView(R.id.mt_result);
+        open.setText(bean.description);
+        ImageView openImg = holder.getView(R.id.img_pic);
+        if (!bean.imgs.isEmpty()) {
+            Glide.with(openImg.getContext()).load(bean.imgs.get(0)).into(openImg);
+        }
+        close.isOpen = bean.flag;
+        if (bean.flag) {
+            close.setVisibility(View.GONE);
+            open.setVisibility(View.VISIBLE);
+        } else {
+            close.setVisibility(View.VISIBLE);
+            open.setVisibility(View.GONE);
+        }
+
+        close.setClickListner(new ExpandableTextView.ClickListner() {
             @Override
-            public void onclick(boolean open) {
-                getData().get(holder.getPosition()).flag=open;
+            public void onclick(boolean isOpen) {
+                getData().get(holder.getPosition()).flag = isOpen;
+                if (isOpen) {
+                    close.setVisibility(View.GONE);
+                    open.setVisibility(View.VISIBLE);
+                } else {
+                    close.setVisibility(View.VISIBLE);
+                    open.setVisibility(View.VISIBLE);
+                }
+
             }
         });
     }
