abf26e6b887c46d4cc7bb610819b84206c05d81f
zhangjiahuashan <1191547491@qq.com>
2018-03-12 13:19:02 +0800

    个人中心加指示点

--- app/src/main/java/com/example/han/referralproject/personal/PersonDetailFragmentPagerAdapter.java
+++ app/src/main/java/com/example/han/referralproject/personal/PersonDetailFragmentPagerAdapter.java
@@ -7,7 +7,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import java.util.List;
 
 public class PersonDetailFragmentPagerAdapter extends FragmentPagerAdapter {
-    private FragmentManager mfragmentManager;
     private List<Fragment> data;
 
     public PersonDetailFragmentPagerAdapter(FragmentManager fm, List<Fragment> list) {
