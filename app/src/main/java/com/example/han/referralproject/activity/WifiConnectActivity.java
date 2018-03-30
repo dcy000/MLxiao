7880c08578fc617f777911228ce5f8cb662968a0
zhangjiahuashan <1191547491@qq.com>
2018-03-30 16:05:39 +0800

    wifi名称

--- app/src/main/java/com/example/han/referralproject/activity/WifiConnectActivity.java
+++ app/src/main/java/com/example/han/referralproject/activity/WifiConnectActivity.java
@@ -57,7 +57,7 @@ public class WifiConnectActivity extends BaseActivity implements View.OnClickLis
                 && cm.getActiveNetworkInfo().getType() == ConnectivityManager.TYPE_WIFI;
         if(iswifiConnected){
 //            mediaPlayer=MediaPlayer.create(this,R.raw.wifi_connected);
-            speak("主人,您的Wifi已连接,如果需要更换,请点击对应wifi名称");
+            speak("主人,您的wifi已连接,如果需要更换,请点击对应wifi名称");
         }else{
 //            mediaPlayer = MediaPlayer.create(this, R.raw.wifi_connect);
             speak("主人,请连接wifi,如果未找到,请点击右上角的刷新按钮");
