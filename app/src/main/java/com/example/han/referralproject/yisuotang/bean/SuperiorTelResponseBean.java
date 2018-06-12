a0eed8e9d20441256dd8922de559f0f5ffffa4e3
zhangjiahuashan <1191547491@qq.com>
2018-06-12 09:59:09 +0800

    邀请码的返回

---
+++ app/src/main/java/com/example/han/referralproject/yisuotang/bean/SuperiorTelResponseBean.java
@@ -0,0 +1,22 @@
+package com.example.han.referralproject.yisuotang.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by lenovo on 2018/6/12.
+ */
+
+public class SuperiorTelResponseBean implements Serializable {
+
+    /**
+     * tag : true
+     * code : 200
+     * data : 12345678911
+     * message : 成功
+     */
+
+    public boolean tag;
+    public int code;
+    public String data;
+    public String message;
+}
