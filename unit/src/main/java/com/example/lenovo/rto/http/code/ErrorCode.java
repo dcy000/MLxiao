eeb45298601562fa578dd87076fba67cbb748ac3
zhangjiahuashan <1191547491@qq.com>
2018-06-20 09:36:56 +0800

    log

--- unit/src/main/java/com/example/lenovo/rto/http/code/ErrorCode.java
+++ unit/src/main/java/com/example/lenovo/rto/http/code/ErrorCode.java
@@ -1,6 +1,8 @@
 package com.example.lenovo.rto.http.code;
 
 
+import android.util.Log;
+
 /**
  * Created by huyin on 2017/9/17.
  */
@@ -8,61 +10,62 @@ package com.example.lenovo.rto.http.code;
 public class ErrorCode {
 
     public static int errorCode;
+    public static String TAG = "BaiDu_yuyin_errorCode";
 
     public static void handleHeaderCode(int headerCode) {
-//          switch (headerCode) {
-//               case 1:
-//                    ToastUtils.toast("服务器内部错误，请再次请求");
-//                    break;
-//               case 2:
-//                    ToastUtils.toast("服务暂不可用，请再次请求");
-//                    break;
-//               case 3:
-//                    ToastUtils.toast("调用的API不存在，请检查后重新尝试");
-//                    break;
-//               case 4:
-//                    ToastUtils.toast("集群超限额");
-//                    break;
-//               case 6:
-//                    ToastUtils.toast("无权限访问该用户数据");
-//                    break;
-//               case 17:
-//                    ToastUtils.toast("每天请求量超限额");
-//                    break;
-//               case 18:
-//                    ToastUtils.toast("QPS超限额");
-//                    break;
-//               case 100:
-//                    ToastUtils.toast("无效的access_token参数，请检查后重新尝试");
-//                    break;
-//               case 110:
-//                    ToastUtils.toast("access_token无效");
-//                    break;
-//               case 111:
-//                    ToastUtils.toast("access token过期");
-//                    break;
-//               case 282000:
-//                    ToastUtils.toast("服务器内部错误，请再次请求");
-//                    break;
-//               case 282004:
-//                    ToastUtils.toast("请求参数格式不正确");
-//                    break;
-//               case 282900:
-//                    ToastUtils.toast("必传字段为空");
-//                    break;
-//               case 282901:
-//                    ToastUtils.toast("场景ID校验失败，请确认console中app和场景是否关联");
-//                    break;
-//               case 282902:
-//                    ToastUtils.toast("UNIT环境启动中，请稍后再试");
-//                    break;
-//               case 282903:
-//                    ToastUtils.toast("UNIT系统异常");
-//                    break;
-//               default:
-//                    ToastUtils.toast("未知错误");
-//                    break;
-//          }
+//        switch (headerCode) {
+//            case 1:
+//                Log.d(TAG, "服务器内部错误，请再次请求");
+//                break;
+//            case 2:
+//                Log.d(TAG, "服务暂不可用，请再次请求");
+//                break;
+//            case 3:
+//                Log.d(TAG, "调用的API不存在，请检查后重新尝试");
+//                break;
+//            case 4:
+//                Log.d(TAG, "集群超限额");
+//                break;
+//            case 6:
+//                Log.d(TAG, "无权限访问该用户数据");
+//                break;
+//            case 17:
+//                Log.d(TAG, "每天请求量超限额");
+//                break;
+//            case 18:
+//                Log.d(TAG, "QPS超限额");
+//                break;
+//            case 100:
+//                Log.d(TAG, "无效的access_token参数，请检查后重新尝试");
+//                break;
+//            case 110:
+//                Log.d(TAG, "access_token无效");
+//                break;
+//            case 111:
+//                Log.d(TAG, "access token过期");
+//                break;
+//            case 282000:
+//                Log.d(TAG, "服务器内部错误，请再次请求");
+//                break;
+//            case 282004:
+//                Log.d(TAG, "请求参数格式不正确");
+//                break;
+//            case 282900:
+//                Log.d(TAG, "必传字段为空");
+//                break;
+//            case 282901:
+//                Log.d(TAG, "场景ID校验失败，请确认console中app和场景是否关联");
+//                break;
+//            case 282902:
+//                Log.d(TAG, "UNIT环境启动中，请稍后再试");
+//                break;
+//            case 282903:
+//                Log.d(TAG, "UNIT系统异常");
+//                break;
+//            default:
+//                Log.d(TAG, "未知错误");
+//                break;
+//        }
 
     }
 }
