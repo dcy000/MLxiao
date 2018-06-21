2d709d0e217a14c8af4718ed6c818c5dc19bcfa5
zhangjiahuashan <1191547491@qq.com>
2018-06-21 09:34:15 +0800

    主页注释,去除无用代码

--- app/src/main/java/com/example/han/referralproject/MainActivity.java
+++ app/src/main/java/com/example/han/referralproject/MainActivity.java
@@ -6,33 +6,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.graphics.drawable.ColorDrawable;
 import android.os.BatteryManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
-import android.widget.PopupWindow;
 
 import com.example.han.referralproject.activity.BaseActivity;
-import com.example.han.referralproject.activity.MarketActivity;
 import com.example.han.referralproject.application.MyApplication;
 import com.example.han.referralproject.bean.ClueInfoBean;
-import com.example.han.referralproject.bean.UserInfo;
 import com.example.han.referralproject.constant.ConstantData;
 import com.example.han.referralproject.facerecognition.AuthenticationActivity;
-import com.example.han.referralproject.floatball.MyService;
 import com.example.han.referralproject.floatingball.AssistiveTouchService;
 import com.example.han.referralproject.network.NetworkApi;
 import com.example.han.referralproject.network.NetworkManager;
-import com.example.han.referralproject.personal.PersonActivity;
 import com.example.han.referralproject.personal.PersonDetailActivity;
-import com.example.han.referralproject.recyclerview.DoctorAskGuideActivity;
 import com.example.han.referralproject.speechsynthesis.PinYinUtils;
 import com.example.han.referralproject.speechsynthesis.SpeechSynthesisActivity;
 import com.example.han.referralproject.yisuotang.HealthMallActivity;
@@ -41,11 +31,8 @@ import com.example.han.referralproject.yisuotang.RecreationCenterActivity;
 import com.medlink.danbogh.alarm.AlarmHelper;
 import com.medlink.danbogh.alarm.AlarmList2Activity;
 import com.medlink.danbogh.alarm.AlarmModel;
-
 import com.medlink.danbogh.call2.NimAccountHelper;
 import com.medlink.danbogh.call2.NimCallActivity;
-import com.ml.edu.OldRouter;
-import com.orhanobut.logger.Logger;
 
 import org.litepal.crud.DataSupport;
 
@@ -76,10 +63,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         setContentView(R.layout.activity_main_yisuo_tang);
 
         StatusBarFragment.show(getSupportFragmentManager(), R.id.fl_status_bar);
-
-     /*   mediaPlayer = MediaPlayer.create(this, R.raw.face_register);
-
-        mediaPlayer.start();//播放音乐*/
         mToolbar.setVisibility(View.GONE);
         mImageView1 = (ImageView) findViewById(R.id.robot_con);
 
@@ -156,64 +139,45 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         Intent intent = new Intent();
         switch (v.getId()) {
             case R.id.ll_anim:
+                //暂时未用到
                 v.setVisibility(View.GONE);
                 break;
             case R.id.robot_con:
+                //人机对话
                 intent.setClass(getApplicationContext(), SpeechSynthesisActivity.class);
                 startActivity(intent);
                 break;
             case R.id.person_info:
+                //个人中心
                 intent.setClass(getApplicationContext(), PersonDetailActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.health_test://健康监测
-
+            case R.id.health_test:
+                //健康监测==>先进入人脸识别===>识别成功之后进入  体检项目入口
                 intent.setClass(getApplicationContext(), AuthenticationActivity.class);
                 intent.putExtra("orderid", "0");
                 intent.putExtra("from", "Test");
                 startActivity(intent);
 
+                //直接进入项目检测入口
 //                startActivity(new Intent(mContext, Test_mainActivity.class));
                 break;
-            case R.id.doctor_ask://医生咨询
-//                intent.setClass(getApplicationContext(), DoctorAskGuideActivity.class);
+            case R.id.doctor_ask:
+                //医药咨询
                 intent.setClass(getApplicationContext(), MedicalConsultationActivity.class);
                 startActivity(intent);
                 break;
             case R.id.health_class:
-//                intent.setClass(getApplicationContext(), MarketActivity.class);
+                //健康商城
                 intent.setClass(getApplicationContext(), HealthMallActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.call_family://紧急呼叫家人
-                //呼叫
+            case R.id.call_family:
+                //呼叫家人
                 NimCallActivity.launchNoCheck(this, MyApplication.getInstance().eqid);
-//                NetworkApi.PersonInfo(MyApplication.getInstance().eqid, new NetworkManager.SuccessCallback<UserInfo>() {
-//                    @Override
-//                    public void onSuccess(UserInfo response) {
-//                        if (isFinishing() || isDestroyed()) {
-//                            return;
-//                        }
-//                        NetworkApi.postTelMessage(response.tel, MyApplication.getInstance().userName, new NetworkManager.SuccessCallback<Object>() {
-//                            @Override
-//                            public void onSuccess(Object response) {
-//
-//                            }
-//                        }, new NetworkManager.FailedCallback() {
-//                            @Override
-//                            public void onFailed(String message) {
-//
-//                            }
-//                        });
-//                    }
-//                }, new NetworkManager.FailedCallback() {
-//                    @Override
-//                    public void onFailed(String message) {
-//
-//                    }
-//                });
                 break;
-            case R.id.yule_center://紧急呼叫家人
+            case R.id.yule_center:
+                //娱乐中心
                 startActivity(new Intent(this, RecreationCenterActivity.class));
                 break;
         }
