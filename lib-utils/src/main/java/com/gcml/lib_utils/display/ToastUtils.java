7eb0f9d15aaa998069725a5b2d44ada1500853f4
gzq <774550196@qq.com>
2018-07-23 10:24:30 +0800

    2018/07/23

--- lib-utils/src/main/java/com/gcml/lib_utils/display/ToastUtils.java
+++ lib-utils/src/main/java/com/gcml/lib_utils/display/ToastUtils.java
@@ -2,6 +2,7 @@ package com.gcml.lib_utils.display;
 
 import android.app.Application;
 import android.content.Context;
+import android.view.Gravity;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -80,6 +81,40 @@ public class ToastUtils {
             mToast = Toast.makeText(getApplication(), resId, Toast.LENGTH_LONG);
             TextView v = (TextView) mToast.getView().findViewById(android.R.id.message);
             v.setTextSize(28);
+        } else {
+            mToast.setText(resId);
+        }
+        mToast.show();
+    }
+
+    /**
+     * 短时间显示Toast
+     *
+     * @param message
+     */
+    public static void showShortOnTop(CharSequence message) {
+        if (mToast == null) {
+            mToast = Toast.makeText(getApplication(), message, Toast.LENGTH_SHORT);
+            TextView v = (TextView) mToast.getView().findViewById(android.R.id.message);
+            v.setTextSize(28);
+            mToast.setGravity(Gravity.TOP,0,0);
+        } else {
+            mToast.setText(message);
+        }
+        mToast.show();
+    }
+
+    /**
+     * 短时间显示Toast
+     *
+     * @param resId   资源ID:getResources().getString(R.string.xxxxxx);
+     */
+    public static void showShortOnTop( int resId) {
+        if (mToast == null) {
+            mToast = Toast.makeText(getApplication(), resId, Toast.LENGTH_SHORT);
+            TextView v = (TextView) mToast.getView().findViewById(android.R.id.message);
+            v.setTextSize(28);
+            mToast.setGravity(Gravity.TOP,0,0);
         } else {
             mToast.setText(resId);
         }
