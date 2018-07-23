2ff4d8ca3a299b2c89cf417517794c0e9a1e0052
afirez <afirez@163.com>
2018-07-23 10:58:30 +0800

    0723

---
+++ app/src/main/java/com/example/han/referralproject/health/firstdetect/HealthFirstDetectActivity.java
@@ -0,0 +1,16 @@
+package com.example.han.referralproject.health.firstdetect;
+
+import android.os.Bundle;
+
+import com.example.han.referralproject.R;
+import com.example.han.referralproject.activity.BaseActivity;
+
+public class HealthFirstDetectActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.health_activity_first_detect);
+        
+    }
+}
