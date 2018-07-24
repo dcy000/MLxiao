ed25ac21b8d833d9399ff1f5ecb75954bd71286f
gzq <774550196@qq.com>
2018-07-24 14:09:15 +0800

    2018/07/24 14:08

---
+++ test_all_devices/src/main/java/com/gzq/test_all_devices/health_record/HealthRecordActivity.java
@@ -0,0 +1,16 @@
+package com.gzq.test_all_devices.health_record;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gzq.test_all_devices.R;
+
+
+public class HealthRecordActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_health_recoed);
+    }
+}
