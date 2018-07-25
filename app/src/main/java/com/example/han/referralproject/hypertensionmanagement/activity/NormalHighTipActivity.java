b49c03efd3f3c40ff21b3b5fb3b206c0f4824a60
zhangjiahuashan <1191547491@qq.com>
2018-07-25 14:23:58 +0800

    血压状态提示页面

--- app/src/main/java/com/example/han/referralproject/hypertensionmanagement/activity/NormalHighTipActivity.java
+++ app/src/main/java/com/example/han/referralproject/hypertensionmanagement/activity/NormalHighTipActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import com.example.han.referralproject.R;
 import com.example.han.referralproject.activity.BaseActivity;
@@ -16,32 +15,19 @@ import com.example.han.referralproject.hypertensionmanagement.fragment.WarmNotic
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-/**
- * 疑似患有高血压
- */
-public class HypertensionTipActivity extends BaseActivity implements WarmNoticeFragment.OnButtonClickListener {
-
+public class NormalHighTipActivity extends BaseActivity implements WarmNoticeFragment.OnButtonClickListener {
+    public static final String CONTENT = " 您好,根据系统中显示的三次数据判定,您的血压疑似在正常高值状态,为给您提供更精确预防高血压方案,以下问题需要您认真作答";
     @BindView(R.id.fl_container)
     FrameLayout flContainer;
-    public static final String CONTENT = " 您好，根据系统中显示的3次数据判定,您疑似患有高血压,以下问题需要您认真作答";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hypertension_tip);
+        setContentView(R.layout.activity_normal_high_tip);
         ButterKnife.bind(this);
         initTitle();
         mlSpeak(CONTENT);
         initView();
-
-    }
-
-    private void initTitle() {
-        mToolbar.setVisibility(View.VISIBLE);
-        mTitleText.setText("基 础 信 息 列 表");
-        mRightText.setVisibility(View.GONE);
-        mRightView.setImageResource(R.drawable.white_wifi_3);
-        mRightView.setOnClickListener(v -> startActivity(new Intent(HypertensionTipActivity.this, WifiConnectActivity.class)));
     }
 
     private void initView() {
@@ -49,19 +35,24 @@ public class HypertensionTipActivity extends BaseActivity implements WarmNoticeF
         FragmentTransaction transaction = manager.beginTransaction();
         WarmNoticeFragment fragment = WarmNoticeFragment.getInstance(CONTENT, "下一步");
         fragment.setListener(this);
-        transaction.replace(R.id.fl_container, fragment, "notice").commitAllowingStateLoss();
+        transaction.replace(R.id.fl_container, fragment).commitAllowingStateLoss();
     }
 
+    private void initTitle() {
+        mToolbar.setVisibility(View.VISIBLE);
+        mTitleText.setText("基 础 信 息 列 表");
+        mRightText.setVisibility(View.GONE);
+        mRightView.setImageResource(R.drawable.white_wifi_3);
+        mRightView.setOnClickListener(v -> startActivity(new Intent(NormalHighTipActivity.this, WifiConnectActivity.class)));
+    }
 
     @Override
     public void onFragmentBtnClick() {
-        Toast.makeText(this, "点击了", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     public void onFragmentBtnTimeOut() {
-        Toast.makeText(this, "时间到了", Toast.LENGTH_SHORT).show();
+
     }
-
-
 }
--- app/src/main/java/com/example/han/referralproject/hypertensionmanagement/activity/PressureFlatTipActivity.java
+++ app/src/main/java/com/example/han/referralproject/hypertensionmanagement/activity/PressureFlatTipActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import com.example.han.referralproject.R;
 import com.example.han.referralproject.activity.BaseActivity;
@@ -16,24 +15,27 @@ import com.example.han.referralproject.hypertensionmanagement.fragment.WarmNotic
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-/**
- * 疑似患有高血压
- */
-public class HypertensionTipActivity extends BaseActivity implements WarmNoticeFragment.OnButtonClickListener {
-
+public class PressureFlatTipActivity extends BaseActivity implements WarmNoticeFragment.OnButtonClickListener {
+    public static final String CONTENT = "  您好,根据系统中显示的三次数据判定,您血压偏低,现系统正为您生成预防高血压方案";
     @BindView(R.id.fl_container)
     FrameLayout flContainer;
-    public static final String CONTENT = " 您好，根据系统中显示的3次数据判定,您疑似患有高血压,以下问题需要您认真作答";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hypertension_tip);
+        setContentView(R.layout.activity_normal_high_tip);
         ButterKnife.bind(this);
         initTitle();
         mlSpeak(CONTENT);
         initView();
+    }
 
+    private void initView() {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        WarmNoticeFragment fragment = WarmNoticeFragment.getInstance(CONTENT, "生成健康方案");
+        fragment.setListener(this);
+        transaction.replace(R.id.fl_container, fragment).commitAllowingStateLoss();
     }
 
     private void initTitle() {
@@ -41,27 +43,16 @@ public class HypertensionTipActivity extends BaseActivity implements WarmNoticeF
         mTitleText.setText("基 础 信 息 列 表");
         mRightText.setVisibility(View.GONE);
         mRightView.setImageResource(R.drawable.white_wifi_3);
-        mRightView.setOnClickListener(v -> startActivity(new Intent(HypertensionTipActivity.this, WifiConnectActivity.class)));
+        mRightView.setOnClickListener(v -> startActivity(new Intent(PressureFlatTipActivity.this, WifiConnectActivity.class)));
     }
-
-    private void initView() {
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        WarmNoticeFragment fragment = WarmNoticeFragment.getInstance(CONTENT, "下一步");
-        fragment.setListener(this);
-        transaction.replace(R.id.fl_container, fragment, "notice").commitAllowingStateLoss();
-    }
-
 
     @Override
     public void onFragmentBtnClick() {
-        Toast.makeText(this, "点击了", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     public void onFragmentBtnTimeOut() {
-        Toast.makeText(this, "时间到了", Toast.LENGTH_SHORT).show();
+
     }
-
-
 }
