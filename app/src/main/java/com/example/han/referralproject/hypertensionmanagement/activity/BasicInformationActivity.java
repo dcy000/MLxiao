d134cc6965e0fbb70bca2dd2c5f5afaa475e13a9
zhangjiahuashan <1191547491@qq.com>
2018-07-27 16:25:45 +0800

    高血压等级分流

--- app/src/main/java/com/example/han/referralproject/hypertensionmanagement/activity/BasicInformationActivity.java
+++ app/src/main/java/com/example/han/referralproject/hypertensionmanagement/activity/BasicInformationActivity.java
@@ -2,27 +2,20 @@ package com.example.han.referralproject.hypertensionmanagement.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.han.referralproject.R;
 import com.example.han.referralproject.activity.BaseActivity;
-import com.example.han.referralproject.activity.MyBaseDataActivity;
 import com.example.han.referralproject.activity.WifiConnectActivity;
-import com.example.han.referralproject.bean.UserInfoBean;
-import com.example.han.referralproject.hypertensionmanagement.dialog.TwoChoiceDialog;
 import com.example.han.referralproject.network.NetworkApi;
-import com.example.han.referralproject.network.NetworkManager;
 import com.gcml.lib_utils.display.ToastUtils;
-import com.medlink.danbogh.utils.Utils;
-import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class BasicInformationActivity extends BaseActivity implements TwoChoiceDialog.OnDialogClickListener {
+public class BasicInformationActivity extends BaseActivity {
 
     @BindView(R.id.tv_next_step)
     TextView tvNextStep;
@@ -66,9 +59,8 @@ public class BasicInformationActivity extends BaseActivity implements TwoChoiceD
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.tv_next_step:
-                // TODO: 2018/7/26 去测体重    暂时跳
-//                startActivity();
-                showOriginHypertensionDialog();
+                // TODO: 2018/7/26 去测体重    暂跳 原发高血压问卷
+                startActivity(new Intent(this, OriginHypertensionTipActivity.class));
                 break;
             case R.id.tv_birth_info:
                 break;
@@ -77,22 +69,5 @@ public class BasicInformationActivity extends BaseActivity implements TwoChoiceD
         }
     }
 
-    private void showOriginHypertensionDialog() {
-        TwoChoiceDialog dialog = new TwoChoiceDialog("您是否诊断过原发性高血压且正在进行高血压规范治疗？", "是", "否");
-        dialog.setListener(this);
-        dialog.show(getFragmentManager(), "yuanfa");
-    }
 
-    @Override
-    public void onClickConfirm(String content) {
-        //-->原发高血压做题提示
-        startActivity(new Intent(this, OriginHypertensionTipActivity.class));
-    }
-
-    @Override
-    public void onClickCancel() {
-        // TODO: 2018/7/27 根据网络获取的健康状况结果 4个分支
-        //赞跳高血压界面
-        startActivity(new Intent(this, HypertensionTipActivity.class));
-    }
 }
