396adec65ce5a4ff7ff35d4c30058f2411aa0d44
afirez <afirez@163.com>
2018-08-20 09:52:28 +0800

    rc databinding RxJava2

--- lib-common-core/src/main/java/com/gcml/common/utils/RxUtils.java
+++ lib-common-core/src/main/java/com/gcml/common/utils/RxUtils.java
@@ -3,6 +3,8 @@ package com.gcml.common.utils;
 
 import android.arch.lifecycle.Lifecycle;
 import android.arch.lifecycle.LifecycleOwner;
+import android.databinding.ObservableField;
+import android.support.annotation.NonNull;
 
 import com.gcml.common.repository.http.ApiException;
 import com.gcml.common.repository.http.ApiResult;
@@ -11,8 +13,11 @@ import com.uber.autodispose.AutoDisposeConverter;
 import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
+import io.reactivex.functions.Cancellable;
 import io.reactivex.functions.Function;
 
 /**
@@ -47,5 +52,29 @@ public class RxUtils {
 
     public static <T> AutoDisposeConverter<T> autoDisposeConverter(LifecycleOwner owner, Lifecycle.Event event) {
         return AutoDispose.autoDisposable(AndroidLifecycleScopeProvider.from(owner, event));
+    }
+
+    public static <T> Observable<T> toObservable(@NonNull final ObservableField<T> observableField) {
+        return Observable.create(new ObservableOnSubscribe<T>() {
+            @Override
+            public void subscribe(ObservableEmitter<T> emitter) throws Exception {
+                emitter.onNext(observableField.get());
+                android.databinding.Observable.OnPropertyChangedCallback callback = new android.databinding.Observable.OnPropertyChangedCallback() {
+                    @Override
+                    public void onPropertyChanged(android.databinding.Observable sender, int propertyId) {
+                        if (sender == observableField) {
+                            emitter.onNext(observableField.get());
+                        }
+                    }
+                };
+                emitter.setCancellable(new Cancellable() {
+                    @Override
+                    public void cancel() throws Exception {
+                        observableField.removeOnPropertyChangedCallback(callback);
+                    }
+                });
+                observableField.addOnPropertyChangedCallback(callback);
+            }
+        });
     }
 }
