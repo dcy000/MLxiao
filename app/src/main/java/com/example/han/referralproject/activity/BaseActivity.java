2ed81ff35e912edb6f56a99d423641fbaac02eed
zhangjiahuashan <1191547491@qq.com>
2018-10-18 09:08:45 +0800

    语音优化

--- app/src/main/java/com/example/han/referralproject/activity/BaseActivity.java
+++ app/src/main/java/com/example/han/referralproject/activity/BaseActivity.java
@@ -673,13 +673,13 @@ public class BaseActivity extends AppCompatActivity {
             }
 
             // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
-            recognizer.setParameter(SpeechConstant.VAD_BOS, mTtsSharedPreferences.getString("iat_vadbos_preference", "5000"));
+            recognizer.setParameter(SpeechConstant.VAD_BOS, "5000");
 
             // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
-            recognizer.setParameter(SpeechConstant.VAD_EOS, mTtsSharedPreferences.getString("iat_vadeos_preference", "500"));
+            recognizer.setParameter(SpeechConstant.VAD_EOS, "2000");
 
             // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
-            recognizer.setParameter(SpeechConstant.ASR_PTT, mTtsSharedPreferences.getString("iat_punc_preference", "0"));
+            recognizer.setParameter(SpeechConstant.ASR_PTT, "0");
 
             // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
             // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
