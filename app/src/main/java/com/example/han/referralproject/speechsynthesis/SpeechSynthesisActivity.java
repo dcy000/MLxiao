5ff189345f19cd0d3c96545acc64cba8902bfb93
zhangjiahuashan <1191547491@qq.com>
2018-11-16 11:18:56 +0800

    优化语音

--- app/src/main/java/com/example/han/referralproject/speechsynthesis/SpeechSynthesisActivity.java
+++ app/src/main/java/com/example/han/referralproject/speechsynthesis/SpeechSynthesisActivity.java
@@ -1577,7 +1577,7 @@ public class SpeechSynthesisActivity extends BaseActivity implements View.OnClic
             public void onSuccess(Unit data) {
                 if (data != null)
                     sessionId = data.getSession_id();
-                for (Unit.ActionListBean action : data.getAction_list()) {
+               /* for (Unit.ActionListBean action : data.getAction_list()) {
 
                     if (!TextUtils.isEmpty(action.getSay())) {
                         sb = new StringBuilder();
@@ -1586,6 +1586,15 @@ public class SpeechSynthesisActivity extends BaseActivity implements View.OnClic
 
                 }
                 str1 = sb.toString().replace("<USER-NAME>", "");
+                defaultToke();*/
+                List<Unit.ActionListBean> list = data.getAction_list();
+                if (list != null && list.size() != 0) {
+                    if (list.size() >= 10) {
+                        str1 = list.get(new Random().nextInt(10)).getSay().replace("<USER-NAME>", "");
+                    } else {
+                        str1 = list.get(0).getSay().replace("<USER-NAME>", "");
+                    }
+                }
                 defaultToke();
             }
 
