d89c6a3e254c21ee04731ee7cef09fbccc0934a3
zhangjiahuashan <1191547491@qq.com>
2018-11-19 10:35:15 +0800

    推送 弹框崩溃的问题

--- app/src/main/java/com/example/han/referralproject/jipush/MyReceiver.java
+++ app/src/main/java/com/example/han/referralproject/jipush/MyReceiver.java
@@ -4,15 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 
 import com.example.han.referralproject.bean.MessageBean;
 import com.medlink.danbogh.utils.rxbus.RxBus;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Iterator;
 
 import cn.jpush.android.api.JPushInterface;
 
@@ -38,13 +32,21 @@ public class MyReceiver extends BroadcastReceiver {
 
             } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
                 Logger.d(TAG, "[MyReceiver] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_MESSAGE));
-                RxBus.getDefault().post(new MessageBean(bundle.getString(JPushInterface.EXTRA_TITLE),
-                        bundle.getString(JPushInterface.EXTRA_MESSAGE)));
+                try {
+                    RxBus.getDefault().post(new MessageBean(bundle.getString(JPushInterface.EXTRA_TITLE),
+                            bundle.getString(JPushInterface.EXTRA_MESSAGE)));
+                } catch (Exception e) {
+
+                }
             } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
                 Logger.d(TAG, "[MyReceiver] 接收到推送下来的通知");
-                //发送消息到BaseActivity
-                RxBus.getDefault().post(new MessageBean(bundle.getString(JPushInterface.EXTRA_TITLE),
-                        bundle.getString(JPushInterface.EXTRA_ALERT)));
+                try {
+                    //发送消息到BaseActivity
+                    RxBus.getDefault().post(new MessageBean(bundle.getString(JPushInterface.EXTRA_TITLE),
+                            bundle.getString(JPushInterface.EXTRA_ALERT)));
+                } catch (Exception e) {
+
+                }
             } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
                 Logger.d(TAG, "[MyReceiver] 用户点击打开了通知");
 
