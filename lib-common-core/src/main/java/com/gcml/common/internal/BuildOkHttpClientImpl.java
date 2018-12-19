f4a46e17b9e7bba6d65f8a86c446432790f2b376
afirez <afirez@163.com>
2018-12-19 14:07:52 +0800

    fix(log) add log

--- lib-common-core/src/main/java/com/gcml/common/internal/BuildOkHttpClientImpl.java
+++ lib-common-core/src/main/java/com/gcml/common/internal/BuildOkHttpClientImpl.java
@@ -3,7 +3,6 @@ package com.gcml.common.internal;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 
-import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.gcml.common.api.BuildOkHttpClient;
 import com.gcml.common.http.HttpLogInterceptor;
 import com.google.auto.service.AutoService;
@@ -12,14 +11,17 @@ import java.util.concurrent.TimeUnit;
 
 import me.jessyan.retrofiturlmanager.RetrofitUrlManager;
 import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 
 @AutoService(BuildOkHttpClient.class)
 public class BuildOkHttpClientImpl implements BuildOkHttpClient {
     @Override
     public void buildOkHttpClient(Context context, OkHttpClient.Builder builder) {
         RetrofitUrlManager.getInstance().with(builder);
-        builder.addNetworkInterceptor(new HttpLogInterceptor(null));
+
+        if ((context.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
+            // debug
+            builder.addNetworkInterceptor(new HttpLogInterceptor(null));
+        }
         builder.writeTimeout(10, TimeUnit.SECONDS)
                 .readTimeout(10, TimeUnit.SECONDS)
                 .connectTimeout(10, TimeUnit.SECONDS);
