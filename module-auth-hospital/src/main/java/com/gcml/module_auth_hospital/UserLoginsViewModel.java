64ea1bbed5bdcb9fa1f0826b6fe6aa33da796ae2
zhangjiahuashan <1191547491@qq.com>
2019-01-08 20:59:07 +0800

    登陆注册

---
+++ module-auth-hospital/src/main/java/com/gcml/module_auth_hospital/UserLoginsViewModel.java
@@ -0,0 +1,16 @@
+package com.gcml.module_auth_hospital;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+
+import com.gcml.common.mvvm.BaseViewModel;
+
+/**
+ * Created by lenovo on 2019/1/8.
+ */
+
+public class UserLoginsViewModel extends BaseViewModel{
+    public UserLoginsViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
