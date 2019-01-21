1de07c2e7b27435387f363f20b48da48600b3174
zhangjiahuashan <1191547491@qq.com>
2019-01-21 18:30:33 +0800

    签约,人脸验证

--- module-auth-hospital/src/main/java/com/gcml/module_auth_hospital/ui/register/RegisterSuccessActivity.java
+++ module-auth-hospital/src/main/java/com/gcml/module_auth_hospital/ui/register/RegisterSuccessActivity.java
@@ -1,18 +1,24 @@
 package com.gcml.module_auth_hospital.ui.register;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
 import com.billy.cc.core.component.CC;
+import com.gcml.common.IConstant;
+import com.gcml.common.data.UserEntity;
 import com.gcml.common.utils.DefaultObserver;
 import com.gcml.common.utils.RxUtils;
 import com.gcml.common.utils.display.ToastUtils;
+import com.gcml.common.widget.dialog.LoadingDialog;
 import com.gcml.common.widget.toolbar.ToolBarClickListener;
 import com.gcml.common.widget.toolbar.TranslucentToolBar;
 import com.gcml.module_auth_hospital.R;
+import com.gcml.module_auth_hospital.model.UserRepository;
 
 import java.util.Locale;
 
@@ -20,6 +26,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by lenovo on 2019/1/14.
@@ -84,6 +91,91 @@ public class RegisterSuccessActivity extends AppCompatActivity implements View.O
     public void onClick(View v) {
         if (v.getId() == R.id.tv_auth_register_success_complete) {
 
+        }
+    }
+
+    private void login(String deviceId) {
+        Intent intent = getIntent();
+        if (intent != null) {
+            return;
+        }
+
+        String idcard = intent.getStringExtra("idcard");
+        if ( TextUtils.isEmpty(idcard)){
+            return;
+        }
+
+        repository
+                .signInByIdCard(deviceId, idcard)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(new Consumer<Disposable>() {
+                    @Override
+                    public void accept(Disposable disposable) throws Exception {
+                        showLoading("正在登录...");
+                    }
+                })
+                .doOnTerminate(new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        dismissLoading();
+                    }
+                })
+                .as(RxUtils.autoDisposeConverter(this))
+                .subscribe(new DefaultObserver<UserEntity>() {
+                    @Override
+                    public void onNext(UserEntity user) {
+                        CC.obtainBuilder("com.gcml.zzb.common.push.setTag")
+                                .addParam("userId", user.id)
+                                .build()
+                                .callAsync();
+                        ToastUtils.showLong("登录成功");
+                        toHome();
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        super.onError(throwable);
+                        ToastUtils.showShort(throwable.getMessage());
+                        toLogin();
+                    }
+                });
+    }
+
+    UserRepository repository = new UserRepository();
+
+    private void toHome() {
+        finish();
+//        startActivity(new Intent(this, RegisterSuccessActivity.class));
+        CC.obtainBuilder(IConstant.KEY_INUIRY_ENTRY).build().callAsync();
+    }
+
+    private void toLogin() {
+        //关闭 注册时的 扫面sfz和信息录入页面
+        finish();
+        startActivity(new Intent(this, ScanIdCardRegisterActivity.class));
+    }
+
+    private LoadingDialog mLoadingDialog;
+
+    private void showLoading(String tips) {
+        if (mLoadingDialog != null) {
+            LoadingDialog loadingDialog = mLoadingDialog;
+            mLoadingDialog = null;
+            loadingDialog.dismiss();
+        }
+        mLoadingDialog = new LoadingDialog.Builder(this)
+                .setIconType(LoadingDialog.Builder.ICON_TYPE_LOADING)
+                .setTipWord(tips)
+                .create();
+        mLoadingDialog.show();
+    }
+
+    private void dismissLoading() {
+        if (mLoadingDialog != null) {
+            LoadingDialog loadingDialog = mLoadingDialog;
+            mLoadingDialog = null;
+            loadingDialog.dismiss();
         }
     }
 }
