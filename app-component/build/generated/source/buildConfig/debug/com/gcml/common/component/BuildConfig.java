25139836fba5de8ef7a15f8c52e6d7e7c42351c8
GZQ <774550196@qq.com>
2019-01-26 15:41:45 +0800

    2019/01/26 15:41

---
+++ app-component/build/generated/source/buildConfig/debug/com/gcml/common/component/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.gcml.common.component;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.gcml.common.component";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 24;
+  public static final String VERSION_NAME = "Robot.1.3.0.beta";
+  // Fields from build type: debug
+  public static final String API_BAIDUBCE = "https://aip.baidubce.com/";
+  public static final String API_SENIVERSE = "https://api.seniverse.com/v3/";
+  public static final boolean RUN_AS_APP = true;
+  public static final String SERVER_ADDRESS = "http://47.96.98.60:8080/";
+}
