b62c4417b4ba57be44ad9b7196d47f2cc55e5417
afirez <afirez@163.com>
2019-01-31 10:46:05 +0800

    fix ( face ): fix face loop create group

--- lib-auth-face/src/main/java/com/gcml/auth/face/model/FaceRepository.java
+++ lib-auth-face/src/main/java/com/gcml/auth/face/model/FaceRepository.java
@@ -236,7 +236,7 @@ public class FaceRepository {
                     public ObservableSource<? extends String> apply(Throwable error) throws Exception {
                         if (error instanceof FaceError) {
                             if (((FaceError) error).getCode() == ERROR_JOIN_GROUP_FACE_EXIST) {
-                                return createAndJoinGroup(faceId);
+                                return Observable.just(groupId);
                             }
 
                             if (((FaceError) error).getCode() == ERROR_ON_JOIN_GROUP_NOT_EXIST) {
