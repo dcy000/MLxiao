c32c55539ed58d9f38992479cdbb39d9479b5942
gzq <774550196@qq.com>
2019-03-26 14:19:23 +0800

    update

---
+++ app-component/build/generated/source/buildConfig/debug/com/gcml/common/demo/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.gcml.common.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.gcml.common.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 29;
+  public static final String VERSION_NAME = "Robot-Demo.1.0.0.1.beta";
+  // Fields from build type: debug
+  public static final String API_BAIDUBCE = "https://aip.baidubce.com/";
+  public static final String API_SENIVERSE = "https://api.seniverse.com/v3/";
+  public static final boolean RUN_AS_APP = true;
+  public static final String SERVER_ADDRESS = "http://47.96.98.60:8030/";
+}
