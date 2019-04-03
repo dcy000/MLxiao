d986353b43c0a43f7247b17a0f797846bf3b1949
gzq <774550196@qq.com>
2019-04-03 10:23:59 +0800

    修复订单重复的bug

--- lib-music-player/src/main/java/com/example/han/referralproject/new_music/LibMusicPlayer.java
+++ lib-music-player/src/main/java/com/example/han/referralproject/new_music/LibMusicPlayer.java
@@ -2,6 +2,7 @@ package com.example.han.referralproject.new_music;
 
 import android.content.Context;
 
+import com.gcml.common.OkHttpClientHelper;
 import com.zhy.http.okhttp.OkHttpUtils;
 
 import java.util.concurrent.TimeUnit;
@@ -20,12 +21,15 @@ public class LibMusicPlayer {
     }
 
     private static void initOkHttpUtils() {
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .connectTimeout(10, TimeUnit.SECONDS)
-                .readTimeout(10, TimeUnit.SECONDS)
-                .writeTimeout(10, TimeUnit.SECONDS)
-                .addInterceptor(new HttpInterceptor())
-                .build();
+
+//        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .connectTimeout(10, TimeUnit.SECONDS)
+//                .readTimeout(10, TimeUnit.SECONDS)
+//                .writeTimeout(10, TimeUnit.SECONDS)
+//                .addInterceptor(new HttpInterceptor())
+//                .build();
+        OkHttpClient okHttpClient = OkHttpClientHelper.get();
+        okHttpClient.newBuilder().addInterceptor(new HttpInterceptor());
         OkHttpUtils.initClient(okHttpClient);
     }
 }
