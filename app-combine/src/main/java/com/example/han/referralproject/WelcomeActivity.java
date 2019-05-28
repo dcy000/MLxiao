d065c14528eb270f265e498740d3d22ff797441e
afirez <afirez.io@gmail.com>
2019-05-28 18:03:30 +0800

    fix me: UserInfo

--- app-combine/src/main/java/com/example/han/referralproject/WelcomeActivity.java
+++ app-combine/src/main/java/com/example/han/referralproject/WelcomeActivity.java
@@ -24,8 +24,8 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        RetrofitUrlManager.getInstance().setGlobalDomain("http://192.168.200.210:5555/");//娄
-//        RetrofitUrlManager.getInstance().setGlobalDomain("http://192.168.200.222:5555/");//左
+//        RetrofitUrlManager.getInstance().setGlobalDomain("http://192.168.200.210:5555/");//娄
+        RetrofitUrlManager.getInstance().setGlobalDomain("http://192.168.200.222:5555/");//左
         initContentView();
     }
 
