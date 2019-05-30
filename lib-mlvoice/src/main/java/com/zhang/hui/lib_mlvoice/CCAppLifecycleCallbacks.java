4221882f30647b0a241b5e478b8a12e8afe297f2
gzq <774550196@qq.com>
2019-05-30 15:42:21 +0800

    测试线上打包

--- lib-mlvoice/src/main/java/com/zhang/hui/lib_mlvoice/CCAppLifecycleCallbacks.java
+++ lib-mlvoice/src/main/java/com/zhang/hui/lib_mlvoice/CCAppLifecycleCallbacks.java
@@ -2,9 +2,7 @@ package com.zhang.hui.lib_mlvoice;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.pm.ApplicationInfo;
 
-import com.billy.cc.core.component.CC;
 import com.gcml.common.api.AppLifecycleCallbacks;
 import com.google.auto.service.AutoService;
 
@@ -17,10 +15,10 @@ public class CCAppLifecycleCallbacks implements AppLifecycleCallbacks {
 
     @Override
     public void onCreate(Application app) {
-        boolean debug = (app.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
-        CC.enableVerboseLog(debug);
-        CC.enableDebug(debug);
-        CC.init(app);
+//        boolean debug = (app.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+//        CC.enableVerboseLog(debug);
+//        CC.enableDebug(debug);
+//        CC.init(app);
 //        CC.enableRemoteCC(true);
     }
 
