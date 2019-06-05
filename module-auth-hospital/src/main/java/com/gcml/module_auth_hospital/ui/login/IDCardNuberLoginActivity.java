ee354d66c8b9f9efb528d09ddd0b6df1d6dfdff9
zhangjiahuashan <1191547491@qq.com>
2019-06-05 15:30:56 +0800

    登录校验

--- module-auth-hospital/src/main/java/com/gcml/module_auth_hospital/ui/login/IDCardNuberLoginActivity.java
+++ module-auth-hospital/src/main/java/com/gcml/module_auth_hospital/ui/login/IDCardNuberLoginActivity.java
@@ -272,7 +272,10 @@ public class IDCardNuberLoginActivity extends ToolbarBaseActivity implements Vie
 
         @Override
         public void afterTextChanged(Editable s) {
-            if (!TextUtils.isEmpty(ccetPhone.getText().toString()) && (!Utils.checkIdCard1(etPsw.getText().toString()))) {
+            if (!TextUtils.isEmpty(ccetPhone.getText().toString())
+                    && (!Utils.checkIdCard1(ccetPhone.getText().toString()))
+                    && !TextUtils.isEmpty(etPsw.getText().toString())
+            ) {
                 tvNext.setEnabled(true);
             } else {
                 tvNext.setEnabled(false);
