2a5be22a5aa0e352f472d3b2de83faa50d4798bb
zhangjiahuashan <1191547491@qq.com>
2019-06-11 11:32:23 +0800

    wifi 图标

--- app-xa/src/main/java/com/example/han/referralproject/HomeAndQianYueActivity.java
+++ app-xa/src/main/java/com/example/han/referralproject/HomeAndQianYueActivity.java
@@ -2,10 +2,10 @@ package com.example.han.referralproject;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.gcml.common.router.AppRouter;
+import com.gcml.common.utils.base.ToolbarBaseActivity;
 import com.gcml.common.widget.toolbar.ToolBarClickListener;
 import com.gcml.common.widget.toolbar.TranslucentToolBar;
 import com.gcml.lib_widget.EclipseImageView;
@@ -13,7 +13,7 @@ import com.sjtu.yifei.annotation.Route;
 import com.sjtu.yifei.route.Routerfit;
 
 @Route(path = "/app/homepage/mainorqianyue/activity")
-public class HomeAndQianYueActivity extends AppCompatActivity implements View.OnClickListener {
+public class HomeAndQianYueActivity extends ToolbarBaseActivity implements View.OnClickListener {
 
     private EclipseImageView qianyue;
     private EclipseImageView home;
@@ -21,6 +21,7 @@ public class HomeAndQianYueActivity extends AppCompatActivity implements View.On
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        isShowToolbar = false;
         setContentView(R.layout.activity_home_or_qianyue);
         initView();
     }
@@ -42,6 +43,7 @@ public class HomeAndQianYueActivity extends AppCompatActivity implements View.On
                     }
                 });
 
+        setWifiLevel(title);
         qianyue = findViewById(R.id.iv_home);
         home = findViewById(R.id.iv_qianyue);
         qianyue.setOnClickListener(this);
@@ -51,6 +53,7 @@ public class HomeAndQianYueActivity extends AppCompatActivity implements View.On
 
     @Override
     public void onClick(View v) {
+        super.onClick(v);
         if (v == qianyue) {
             //签约
             Routerfit.register(AppRouter.class).skipBindDoctorActivity();
